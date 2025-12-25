3cb60adc786bc6566e06ad4f2d60f8cdc3a98241
ferreirafabian <ferreirafabianjavier2023@gmail.com>
2025-12-25 04:00:59 -0300

    Update urls.py

--- blog_kms/users/urls.py
+++ blog_kms/users/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('perfil/', views.perfil, name='perfil'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='users/password_change.html',
+            success_url='/'  # redirige al inicio después de cambiar la contraseña
+        ),
+        name='password_change'
+    ),
 ]
